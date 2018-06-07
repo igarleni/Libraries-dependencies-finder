3dcdd1e4394efddf697de76a7eb26f64bc8a29e1
italogar6 <italogar6@hotmail.com>
2018-06-07 11:18:31 +0200

    new version released, now it updates on remote with only 1 function executed on local

--- R/InterfaceFunctions.R
+++ R/InterfaceFunctions.R
@@ -34,7 +34,7 @@ updateDependencies <- function(newLibraries, outputFileName, fileName = NULL)
 #'  Then, it gets the version of each package on that column and generates a new
 #'  column with this data. After this, saves this file on the path defined by 
 #'  user.
-#' @export
+#' @keywords internal
 #' @param fileName the path to a csv with libraries.
 #' @param outputFileName path of the output csv file.
 #' @author Italo Garleni
@@ -45,13 +45,13 @@ updateVersion <- function(fileName, outputFileName)
   write.csv2(libraries, outputFileName, quote = F, row.names = F)
 }
 
-#' @name updateDependenciesOnServer
-#' @title (main) Add libraries to Libraries list or initiazlite it on remote server.
-#' @description Function reads a csvFile with almost 1 column named "Package"
+#' @name remoteUpdateMiniCran
+#' @title (main) Updates miniCran file on a remote server.
+#' @description Function download a csvFile with almost 1 column named "Package"
 #'  (if provided), and a vector of libraries. Then, it executes getDependencies
-#'  function with both libraries lists joined, and the result on a new csv file.
-#'  It also test the results for better reliability. The csv file returned has 2
-#'  columns: numeric id and "Package".
+#'  function locally with both libraries lists joined, and uploadthe resulting
+#'  csv file on the remote server. Finally, it updates the version column with
+#'  libraries version on this server.
 #' @export
 #' @param newLibraries a list of new libraries the user wants to add to the
 #'  previous one.
@@ -62,10 +62,11 @@ updateVersion <- function(fileName, outputFileName)
 #' @param sshURL url to remote host (IP not permitted).
 #' @param remoteOldFile (optional) path to a remote csv with libraries.
 #' @author Italo Garleni
-updateDependenciesOnServer <- function(newLibraries, remoteOutputFullPath,
+remoteUpdateMiniCran <- function(newLibraries, remoteOutputFullPath,
                                        userName, password, sshURL,
                                        remoteOldFile = NULL)
 {
+  testServerConnection(userName, password, sshURL)
   if(is.null(remoteOldFile))
   {
     libraries <- newLibraries
@@ -80,4 +81,7 @@ updateDependenciesOnServer <- function(newLibraries, remoteOutputFullPath,
   librariesData <- data.frame(Package = librariesWithDependencies)
   writeCSVToRemote(librariesData, remoteOutputFullPath, userName, password,
                        sshURL)
+  rExpression <- paste0("DependenciesFinder::updateVersion('",
+                        remoteOutputFullPath, "', '", remoteOutputFullPath, "')")
+  runRCodeOnRemote(rExpression, userName, password, sshURL)
 }
--- R/sshUtils.R
+++ R/sshUtils.R
@@ -3,21 +3,18 @@
 #' @description Function that parses a csv file from a remote server, and 
 #'  and convert it to data.frame.
 #' @keywords internal
-#' @param fileFullPath remote file path and name.
+#' @param remoteFullPath remote file path and name.
 #' @param userName user name on remote host.
 #' @param password user password on remote host.
 #' @param sshURL url to remote host (IP not permitted).
 #' @return a data.frame
 #' @author Italo Garleni
-readCSVFromRemote <- function(fileFullPath, userName, password, sshURL)
+readCSVFromRemote <- function(remoteFullPath, userName, password, sshURL)
 {
   tempPath <- tempdir()
-  scpQuery <- paste0("pscp -pw ", password," ", userName, "@", sshURL, ":",
-                     fileFullPath, " ", tempPath)
-  system(scpQuery)
-  
-  fileName <- basename(fileFullPath)
-  tempFileFullPath <- paste0(tempPath,"/", fileName)
+  downloadFromRemote(tempPath, remoteFullPath, userName, password, sshURL)
+  fileName <- basename(remoteFullPath)
+  tempFileFullPath <- paste0(tempPath, "/", fileName)
   inputData <- read.csv(tempFileFullPath, sep = ";", stringsAsFactors = FALSE)
   file.remove(tempFileFullPath)
   return(inputData)
@@ -40,10 +37,41 @@ writeCSVToRemote <- function(inputData, remoteFullPath, userName, password,
 {
   tempPath <- tempdir()
   fileName <- basename(remoteFullPath)
-  tempFileFullPath <- paste0(tempPath,"/", fileName)
+  tempFileFullPath <- paste0(tempPath, "/", fileName)
   write.csv2(inputData, tempFileFullPath, quote = F, row.names = F)
   uploadToRemote(tempFileFullPath, remoteFullPath, userName, password, sshURL)
   file.remove(tempFileFullPath)
+}
+
+#' @name testServerConnection
+#' @title test server connection.
+#' @description Function that upload file on a remote server and then remove it.
+#' @keywords internal
+#' @param userName user name on remote host.
+#' @param password user password on remote host.
+#' @param sshURL url to remote host (IP not permitted).
+#' @author Italo Garleni
+testServerConnection <- function(userName, password, sshURL)
+{
+  tempPath <- tempdir()
+  fileName <- "connectionTest"
+  tempFileFullPath <- paste0(tempPath, "/", fileName)
+  write("connectionTest", tempFileFullPath)
+  tryCatch(
+    {
+      uploadToRemote(tempFileFullPath, "", userName, password,
+                     sshURL)
+      deleteFromRemote(fileName, userName, password, sshURL)
+    },
+    error = function(e)
+    {
+      stop("Can't connect to server. Check username/password/URL.", call. = F)
+    },
+    finally = 
+    {
+      file.remove(tempFileFullPath)
+    }
+  )
 }
 
 #' @name uploadToRemote
@@ -58,14 +86,90 @@ writeCSVToRemote <- function(inputData, remoteFullPath, userName, password,
 #' @param sshURL url to remote host (IP not permitted).
 #' @author Italo Garleni
 uploadToRemote <- function(localFullPath, remoteFullPath, userName, 
-                               password, sshURL)
+                           password, sshURL)
 {
-  scpQuery <- paste0("pscp -pw ", password," ", localFullPath, " ", userName,
+  scpQuery <- paste0("pscp -pw ", password, " ", localFullPath, " ", userName,
                      "@", sshURL, ":", remoteFullPath)
-  haveErrors <- system(scpQuery)
-  if(!haveErrors)
-    file.remove(fileName)
-  else
+  haveErrors <- suppressWarnings(system(scpQuery))
+  if(haveErrors)
     stop(paste("Fail on upload to remote server. You can find the file on",
                localFullPath), call. = FALSE)
+}
+
+
+#' @name downloadFromRemote
+#' @title pull file from server.
+#' @description Function that download file on a remote server.
+#' @keywords internal
+#' @param localFullPath local path to downolad in.
+#' @param remoteFullPath remote file path and name (without "~/" or "/" if you
+#'  want to start from your home direcory).
+#' @param userName user name on remote host.
+#' @param password user password on remote host.
+#' @param sshURL url to remote host (IP not permitted).
+#' @author Italo Garleni
+downloadFromRemote <- function(localFullPath, remoteFullPath, userName, 
+                           password, sshURL)
+{
+  scpQuery <- paste0("pscp -pw ", password," ", userName, "@", sshURL, ":",
+                     remoteFullPath, " ", localFullPath)
+  haveErrors <- suppressWarnings(system(scpQuery))
+  if(haveErrors)
+    stop("Fail on download to remote server.", call. = FALSE)
+}
+
+#' @name deleteFromRemote
+#' @title remove file from server.
+#' @description Function that delete file from a remote server.
+#' @keywords internal
+#' @param remoteFullPath remote file path and name (without "~/" or "/" if you
+#'  want to start from your home direcory).
+#' @param userName user name on remote host.
+#' @param password user password on remote host.
+#' @param sshURL url to remote host (IP not permitted).
+#' @author Italo Garleni
+deleteFromRemote <- function(remoteFullPath, userName, password, sshURL)
+{
+  deleteQuery <- paste0("plink -ssh -pw ", password, " ", userName,
+                        "@", sshURL, " rm ", remoteFullPath)
+  haveErrors <- suppressWarnings(system(deleteQuery))
+  if(haveErrors)
+    stop("Fail on remove from remote server.", call. = FALSE)
+}
+
+#' @name runRCodeOnRemote
+#' @title runs R code on server.
+#' @description Function runs R code on remote server.
+#' @keywords internal
+#' @param rCode string with R code to evaluate.
+#' @param userName user name on remote host.
+#' @param password user password on remote host.
+#' @param sshURL url to remote host (IP not permitted).
+#' @author Italo Garleni
+runRCodeOnRemote <- function(rCode, userName, password, sshURL)
+{
+  tempPath <- tempdir()
+  fileName <- "tempScript.R"
+  tempFileFullPath <- paste0(tempPath, "/", fileName)
+  write(rCode, tempFileFullPath)
+  tryCatch(
+    {
+      uploadToRemote(tempFileFullPath, "", userName, password, sshURL)
+      rScriptQuery <- paste0("plink -ssh -pw ", password, " ", userName,
+                             "@", sshURL, " Rscript ", fileName)
+      haveErrors <- suppressWarnings(system(rScriptQuery))
+      deleteFromRemote(fileName, userName, password, sshURL)
+      if(haveErrors)
+        stop(call. = FALSE)
+    },
+    error = function(e)
+    {
+      stop("Fail on running code on remote server.", call. = FALSE)
+    },
+    finally =
+    {
+      file.remove(tempFileFullPath)
+    }
+  )
+  
 }
